a7b7eb4cc23591e498ae5208bea6fd80269daefb
Akhila499 <akhilathota499@gmail.com>
2022-09-23 00:21:39 -0400

    initial steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useGetAllCharactersQuery, useGetFilmByIdQuery } from './services/post'; 
 
 function App() {
+  const responseInfo = useGetAllCharactersQuery();
+  const responseInfo1 = useGetFilmByIdQuery(3);
+
+  console.log("response info", responseInfo.data);
+  console.log("response info!!!!", responseInfo1.data);
+  if(responseInfo.isLoading) return <div>Loading....</div>
+  if(responseInfo.isError) return <div>An error occured...{responseInfo.error.error}</div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <select>
+        {responseInfo.data.results.map((option, i) => (
+          <option key={i} value={option.name}>{option.name}</option>
+        ))}
+      </select>
     </div>
   );
 }
